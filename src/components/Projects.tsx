0621d04b4a9cb83bf994190954a7831c34fde50d
Fitcrates <aeonofshreds@gmail.com>
2025-01-02 17:58:28 +0100

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -83,7 +83,7 @@ const Projects = () => {
             {/* Top Image Section */}
             <div className="h-2/5 relative overflow-hidden bg-[#140F2D] transform hover:scale-110 transition duration-300">
               <img
-                src="..\public\media\moja strona2.png"
+                src="\media\moja strona2.png"
                 alt="Custom Website"
                 className="w-full h-full object-cover"
               />
@@ -127,7 +127,7 @@ const Projects = () => {
             {/* Top Image Section */}
             <div className="h-2/5 relative overflow-hidden bg-[#140F2D] transform hover:scale-110 transition duration-300">
               <img
-                src="..\public\media\flixapp.png"
+                src="\media\flixapp.png"
                 alt="Mobile App"
                 className="w-full h-full object-cover"
               />
@@ -175,7 +175,7 @@ const Projects = () => {
             {/* Top Image Section */}
             <div className="h-2/5 relative overflow-hidden bg-[#140F2D] transform hover:scale-110 transition duration-300">
               <img
-                src="..\public\media\glide1.png"
+                src="\media\glide1.png"
                 alt="Design"
                 className="w-full h-full object-cover"
               />
