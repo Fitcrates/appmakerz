5ea2aa5ae4859ec11cc6497d371c641d69353c9b
Fitcrates <aeonofshreds@gmail.com>
2025-01-07 07:44:10 +0100

    Update Pricing.tsx

--- src/Pricing.tsx
+++ src/Pricing.tsx
@@ -90,7 +90,7 @@ const Pricing = () => {
       <main>
         <section
           id="pricing-hero"
-          className="hero-section min-h-screen h-screen w-full flex items-end pb-24 overflow-x-hidden"
+          className="hero-section min-h-screen h-screen w-full flex items-end pb-12 overflow-x-hidden"
         >
           <div className="max-w-7xl mx-auto w-full flex flex-col justify-end px-4 sm:px-6 lg:px-8 h-full">
             {/* Main Content */}
