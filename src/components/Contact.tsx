614b9899163d40c1c54a9ac90302262c75bd976b
Fitcrates <aeonofshreds@gmail.com>
2025-01-08 09:11:38 +0100

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -20,7 +20,7 @@ const Contact: React.FC<ContactProps> = ({ id }) => {
       className="py-20"
       style={{ backgroundColor: '#140F2D' }}
     >
-      <Toaster position="top-right" />
+      <Toaster position="center" />
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           <div className="md:col-span-2">
