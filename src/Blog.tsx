f6eb33115c9d4565559dd7537aa54a1e033fe429
Fitcrates <aeonofshreds@gmail.com>
2025-01-09 12:41:58 +0100

    Update Blog.tsx

--- src/Blog.tsx
+++ src/Blog.tsx
@@ -118,89 +118,83 @@ const Blog = () => {
       <Header />
       <main className="relative">
         {/* Hero Section */}
-        <section
-        id="blog-hero"
-        className="hero-section  min-h-screen h-screen w-full flex items-end pb-12 overflow-x-hidden"
-      >
-        <div className="max-w-7xl mx-auto w-full flex flex-col justify-end px-4 sm:px-6 lg:px-8 h-full">
-          {/* Main Content */}
-          <div className="flex flex-col md:flex-row justify-between items-center md:items-end w-full">
-            {/* Centered Text Section */}
-            <div className="flex flex-col items-center text-center md:items-start md:text-left">
-              {/* Large White Headings */}
-              <h1 className="text-5xl sm:text-7xl md:text-8xl font-light text-white tracking-tight font-jakarta font-normal leading-snug -mt-4">
-                {t.header1}
-              </h1>
-              <h1 className="text-5xl sm:text-7xl md:text-8xl font-light text-white tracking-tight font-jakarta font-normal leading-snug -mt-4">
-                {t.header2}
-              </h1>
-            </div>
-
-            {/* Desktop View: Black Subtext and Arrow in Two Columns */}
-            <div className="hidden md:flex flex-row items-center justify-end w-full mt-20 md:mt-16 -mb-20 -space-x-6 md:-space-x-12">
-              {/* Text Column */}
-              <div className="text-left flex flex-col leading-loose">
-                <span className="text-lg sm:text-xl md:text-3xl text-black tracking-normal font-jakarta font-extralight -mt-1">
-                  {t.header3}
-                </span>
-                <span className="text-lg sm:text-xl md:text-3xl text-black tracking-normal font-jakarta font-extralight -mt-1">
-                  {t.header4}
-                </span>
-                <span className="text-lg sm:text-xl md:text-3xl text-black tracking-normal font-jakarta font-extralight -mt-1">
-                  {t.header5}
-                </span>
-                <span className="text-lg sm:text-xl md:text-3xl text-black tracking-normal font-jakarta font-extralight -mt-1">
-                  {t.header6}
-                </span>
+        <section className="hero-section min-h-screen h-screen w-full flex items-end pb-12 overflow-x-hidden">
+          <div className="max-w-7xl mx-auto w-full flex flex-col justify-end px-4 sm:px-6 lg:px-8 h-full">
+            <div className="flex flex-col md:flex-row justify-between items-center md:items-end w-full">
+              {/* Text Container */}
+              <div className="flex flex-col items-center text-center md:items-start md:text-left">
+                <h1 className="text-5xl sm:text-7xl md:text-8xl font-light text-white tracking-tight font-jakarta font-normal leading-snug -mt-1">
+                  {t.header1}
+                </h1>
+                <h1 className="text-5xl sm:text-7xl md:text-8xl font-light text-white tracking-tight font-jakarta font-normal leading-snug -mt-1">
+                  {t.header2}
+                </h1>
               </div>
 
-              {/* Arrow Section */}
-            <div
-              className="flex items-end md:ml-8 mt-6 md:mt-0 cursor-pointer"
-              onMouseEnter={() => setIsHovered(true)} // Handle hover start
-              onMouseLeave={() => setIsHovered(false)} // Handle hover end
-            >
-              <a
-                href="#blog-posts"
-                onClick={(e) => {
-                  e.preventDefault();
-                  document.getElementById('blog-posts')?.scrollIntoView({ behavior: 'smooth' });
-                }}
-                className="text-white hover:text-indigo-500 text-shadow-fuchsia transform transition-transform duration-1000 flex items-center"
-              >
-                {isHovered ? (
-                  <ArrowDown
-                    className="w-20 h-72 sm:w-72 sm:h-72 md:w-72 md:h-72 mb-0 transition-transform duration-1000 transform rotate-360"
-                    strokeWidth={0.7}
-                    strokeLinecap="butt"
-                  />
-                ) : (
-                  <ArrowUpRight
-                    className="w-20 h-72 sm:w-72 sm:h-72 md:w-72 md:h-72 mb-0 transition-transform duration-1000"
-                    strokeWidth={0.7}
-                    strokeLinecap="butt"
-                  />
-                )}
-              </a>
-            </div>
-          </div>
+              {/* Desktop View: Black Subtext and Arrow */}
+              <div className="hidden md:flex flex-row items-center justify-end w-full mt-20 md:mt-16 -mb-20 -space-x-6 md:-space-x-12">
+                {/* Text Column */}
+                <div className="text-left flex flex-col leading-loose">
+                  <span className="text-lg sm:text-xl md:text-3xl text-black tracking-normal font-jakarta font-extralight -mt-1">
+                    {t.header3}
+                  </span>
+                  <span className="text-lg sm:text-xl md:text-3xl text-black tracking-normal font-jakarta font-extralight -mt-1">
+                    {t.header4}
+                  </span>
+                  <span className="text-lg sm:text-xl md:text-3xl text-black tracking-normal font-jakarta font-extralight -mt-1">
+                    {t.header5}
+                  </span>
+                  <span className="text-lg sm:text-xl md:text-3xl text-black tracking-normal font-jakarta font-extralight -mt-1">
+                    {t.header6}
+                  </span>
+                </div>
 
-            {/* Mobile View: Centered Button */}
-            <div className="md:hidden flex flex-col items-center justify-center w-full mt-12 mb-36">
-              <button
-                onClick={(e) => {
-                  e.preventDefault();
-                  document.getElementById('blog-posts')?.scrollIntoView({ behavior: 'smooth' });
-                }}
-                className="py-3 px-6 bg-teal-300 text-gray-900 rounded-full hover:bg-teal-600 transition-colors duration-300 flex items-center space-x-2 font-jakarta font-medium text-lg"
-              >
-                <span>Learn More</span>
-                <ArrowUpRight className="w-5 h-5" />
-              </button>
+                {/* Arrow Section */}
+                <div
+                  className="flex items-end md:ml-8 mt-6 md:mt-0 cursor-pointer"
+                  onMouseEnter={() => setIsHovered(true)}
+                  onMouseLeave={() => setIsHovered(false)}
+                >
+                  <a
+                    href="#blog-posts"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      document.getElementById('blog-posts')?.scrollIntoView({ behavior: 'smooth' });
+                    }}
+                    className="text-white hover:text-indigo-500 text-shadow-fuchsia transform transition-transform duration-1000 flex items-center"
+                  >
+                    {isHovered ? (
+                      <ArrowDown
+                        className="w-20 h-72 sm:w-72 sm:h-72 md:w-72 md:h-72 mb-0 transition-transform duration-1000 transform rotate-360"
+                        strokeWidth={0.7}
+                        strokeLinecap="butt"
+                      />
+                    ) : (
+                      <ArrowUpRight
+                        className="w-20 h-72 sm:w-72 sm:h-72 md:w-72 md:h-72 mb-0 transition-transform duration-1000"
+                        strokeWidth={0.7}
+                        strokeLinecap="butt"
+                      />
+                    )}
+                  </a>
+                </div>
+              </div>
+
+              {/* Mobile View: Centered Button */}
+              <div className="md:hidden flex flex-col items-center justify-center w-full mt-16 mb-56">
+                <button
+                  onClick={() => {
+                    document.getElementById('blog-posts')?.scrollIntoView({ behavior: 'smooth' });
+                  }}
+                  className="py-2 px-4 bg-teal-300 text-gray-900 rounded-full hover:bg-teal-600 transition-colors duration-300 flex items-center space-x-2 font-jakarta font-normal text-base"
+                >
+                  <span>{t.readMore}</span>
+                  <ArrowDown className="w-4 h-4" />
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
 
         {/* Blog Content Section */}
         <section id="blog-posts" className="py-16 bg-[#140F2D]">
