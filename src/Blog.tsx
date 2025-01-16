69cb2e6b47605193dd85fc0faae09f03bb2b052f
Fitcrates <aeonofshreds@gmail.com>
2025-01-16 13:22:06 +0100

    Update Blog.tsx

--- src/Blog.tsx
+++ src/Blog.tsx
@@ -132,7 +132,7 @@ const Blog = () => {
               </div>
 
               {/* Desktop View: Black Subtext and Arrow */}
-              <div className="hidden md:flex flex-row items-center justify-end w-full mt-20 md:mt-16 -mb-20 -space-x-6 md:-space-x-12">
+              <div className="hidden lg:flex flex-row items-center justify-end w-full mt-20 md:mt-16 -mb-20 -space-x-6 md:-space-x-12">
                 {/* Text Column */}
                 <div className="text-left flex flex-col leading-loose">
                   <span className="text-lg sm:text-xl md:text-3xl text-black tracking-normal font-jakarta font-extralight">
