cfed2bf7fe4f5d1a8ea0844ee0f1e28355fd165d
Fitcrates <aeonofshreds@gmail.com>
2025-02-11 10:00:11 +0100

    Update index.ts

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,5 +1,7 @@
 import project from './project'
 import post from './post'
 import author from './author'
+import subscriber from './subscribers'
+import pages from './pages'
 
-export const schemaTypes = [project, post, author]
+export const schemaTypes = [project, post, author, subscriber, pages]
