966efbbe0aeb1a6396efafd343fbb18b58e44f4a
Fitcrates <aeonofshreds@gmail.com>
2025-02-11 13:07:35 +0100

    Delete schemas directory

--- schemas/index.ts
+++
@@ -1,7 +0,0 @@
-import project from './project'
-import post from './post'
-import author from './author'
-import subscriber from './subscribers'
-import pages from './pages'
-
-export const schemaTypes = [project, post, author, subscriber, pages]
