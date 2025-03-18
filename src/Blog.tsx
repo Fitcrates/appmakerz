9c505c04ffa250084966c10f9f0eef3ba71e4b51
Fitcrates <aeonofshreds@gmail.com>
2025-03-18 13:49:13 +0100

    Update Blog.tsx

--- src/Blog.tsx
+++ src/Blog.tsx
@@ -217,7 +217,7 @@ const Blog = () => {
             </div>
 
             {/* Content Grid */}
-            <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
               {/* Main Content */}
               <div className="lg:col-span-2">
                 {isLoading ? (
